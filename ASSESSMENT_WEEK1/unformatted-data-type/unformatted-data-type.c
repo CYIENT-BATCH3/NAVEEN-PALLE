b6ea30cb9688cdcfab629d3dd65a9ec0de83e5d0
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-26 22:54:35 +0530

    Add files via upload

---
+++ ASSESSMENT_WEEK1/unformatted-data-type/unformatted-data-type.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+//////////////unformate//////////////////////
+char y[10];
+printf(" enter\n");//taking value
+gets(y);//assigning value to variable
+printf("value is ");
+puts(y);//printing value 
+
+
+return 0;
+}
