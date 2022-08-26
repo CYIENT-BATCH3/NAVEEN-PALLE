87e96b521f5c1823fceb75468842ded166c1c2fa
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-26 22:37:58 +0530

    Update struct_exp.c

--- ASSESSMENT_WEEK1/struct_exp.c
+++ ASSESSMENT_WEEK1/struct_exp.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 #include<float.h>
+//declaring structure 
 struct de{
+    //taking some data types
     long int a;
     char b[40];
     float c;
     
 }addition;
 int main(){
-    printf("enter name : \n");
-    scanf("%s",addition.b);
-    printf("enter phone number : ");
-    scanf("%ld",&addition.a);
-    printf("\n enter salary : ");
-    scanf("%f",&addition.c);
+    printf("enter name : \n");//taking input
+    scanf("%s",addition.b);//assigning to variable
+    printf("enter phone number : ");//taking num from user
+    scanf("%ld",&addition.a);//assigning to variable
+    printf("\n enter salary : ");//taking salary value from user
+    scanf("%f",&addition.c);//assigning to variable
     printf("\n ass details is :");
-    printf("\n%s",addition.b);
-    printf("\n%ld \n",addition.a);
-    printf(" %f",addition.c);
+    printf("\n%s",addition.b);//printing output
+    printf("\n%ld \n",addition.a);//printing output
+    printf(" %f",addition.c);//printing output
     return 0;
 }
