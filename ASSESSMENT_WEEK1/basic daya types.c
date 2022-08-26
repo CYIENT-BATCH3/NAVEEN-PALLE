5ad7a4fde8cca8fa33f86ca43988de61cba7d46a
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-26 22:27:57 +0530

    Update basic daya types.c

--- ASSESSMENT_WEEK1/basic daya types.c
+++ ASSESSMENT_WEEK1/basic daya types.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main()
 {
+ //declaring variables
  int a;
  float b;
  char c;
- printf("ENTER THE INTEGER VALUE OF a\n");
- scanf("%d",&a);
- printf("ENTER THE FLOAT VALUE OF b\n");
- scanf("%f",&b);
- printf("ENTER THE CHARACTER VALUE OF c\n");
- scanf(" %c",&c);
- printf("The integer is =:%d\n",a);
- printf("The float is =:%f\n",b);
- printf("The char is =:%c\n",c);
+ printf("ENTER THE INTEGER VALUE OF a\n");//taking input from user
+ scanf("%d",&a);//assign to variable
+ printf("ENTER THE FLOAT VALUE OF b\n");//taking input from user
+ scanf("%f",&b);//assign to variable
+ printf("ENTER THE CHARACTER VALUE OF c\n");//taking input from user
+ scanf(" %c",&c);//assign to variable
+ printf("The integer is =:%d\n",a);//printing output
+ printf("The float is =:%f\n",b);//printing output
+ printf("The char is =:%c\n",c);//printing output
+ return 0;
 }
