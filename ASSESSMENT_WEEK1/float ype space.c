9c3b0c2385e19cc3eb1f800a38bb2a6ee8a8cfe0
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-26 22:33:06 +0530

    Update float ype space.c

--- ASSESSMENT_WEEK1/float ype space.c
+++ ASSESSMENT_WEEK1/float ype space.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include<string.h>
+#include<float.h>
 int main() {  
+    //declaring variable
     float floatType;
-    
+    //printing size of float
    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("&E\n",FLT_MIN);//printing size of min range
+     printf("&E\n",FLT_MAX);//printing size of max range
+     
+    return 0;
 
    
 }
