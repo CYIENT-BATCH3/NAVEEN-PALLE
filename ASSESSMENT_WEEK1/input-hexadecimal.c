84f5c4b1611df9e1afbb9368675a3ee02a9359bd
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-26 22:34:03 +0530

    Update input-hexadecimal.c

--- ASSESSMENT_WEEK1/input-hexadecimal.c
+++ ASSESSMENT_WEEK1/input-hexadecimal.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include<string.h>
 int main() {  
-    char c[20];
-   scanf("%s",c);
-   printf("%s",c);
+    
+    char c[20];//variable declaration
+   scanf("%s",c);//taking input and assigning to variable
+   printf("%s",c);//printing value of c
   // int d = strlen(c);
 
    
