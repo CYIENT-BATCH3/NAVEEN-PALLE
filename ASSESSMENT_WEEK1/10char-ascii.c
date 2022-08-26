25c405853150177c21e730e26d74bb97b9af1425
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-26 22:10:50 +0530

    Update 10char-ascii.c

--- ASSESSMENT_WEEK1/10char-ascii.c
+++ ASSESSMENT_WEEK1/10char-ascii.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-int main() {  
+int main() { 
+    //defining variables
     char c[10];
     int i=0;
+    //taking input from user
     printf("Enter a character: ");
+    //assigning value to a variable
     scanf("%s", &c); 
     //printf("%c", c[1]);
+    //printing string in reverse
     for(i=0;i<=9;i++){
        // printf("%c",c[i]);
        printf("\nthe value of c[%d] = %d", i , c[i]);
