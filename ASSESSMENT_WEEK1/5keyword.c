06e09f274980c6346fb6c32856ded805c7c551ab
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-26 22:19:00 +0530

    Update 5keyword.c

--- ASSESSMENT_WEEK1/5keyword.c
+++ ASSESSMENT_WEEK1/5keyword.c
@@ -3,24 +3,25 @@
 
 int main()
 {
+ //declaring variables
  int a;
  float b;
  char c;
  long d ;
  bool x=0;
- printf("ENTER THE INTEGER VALUE OF a\n");
- scanf("%d",&a);
- printf("ENTER THE FLOAT VALUE OF b\n");
- scanf("%f",&b);
- printf("ENTER THE CHARACTER VALUE OF c\n");
- scanf(" %c",&c);
-  printf("ENTER THE long VALUE OF a\n");
- scanf("%ld",&d);
-  printf("ENTER THE bool VALUE OF a\n");
- scanf("%d",&x);
- printf("The integer is =:%d\n",a);
- printf("The float is =:%f\n",b);
- printf("The char is =:%c\n",c);
- printf("The long is =:%ld\n",d);
- printf("The bool is =:%d\n",x);
+ printf("ENTER THE INTEGER VALUE OF a\n");//taking input from user
+ scanf("%d",&a);//assigning value to variable
+ printf("ENTER THE FLOAT VALUE OF b\n");//taking input from user
+ scanf("%f",&b);//assigning value to variable
+ printf("ENTER THE CHARACTER VALUE OF c\n");//taking input from user
+ scanf(" %c",&c);//assigning value to variable
+  printf("ENTER THE long VALUE OF a\n");//taking input from user
+ scanf("%ld",&d);//assigning value to variable
+  printf("ENTER THE bool VALUE OF a\n");//taking input from user
+ scanf("%d",&x);//assigning value to variable
+ printf("The integer is =:%d\n",a);//printing  output
+ printf("The float is =:%f\n",b);//printing  output
+ printf("The char is =:%c\n",c);//printing  output
+ printf("The long is =:%ld\n",d);//printing  output
+ printf("The bool is =:%d\n",x);//printing  output
 }
