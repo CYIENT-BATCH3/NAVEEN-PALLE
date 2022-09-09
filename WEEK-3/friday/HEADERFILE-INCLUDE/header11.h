6e161dfa159ab2c6a7c385874eb992cc97b6f931
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-09-09 14:26:26 +0530

    Create header11.h

---
+++ WEEK-3/friday/HEADERFILE-INCLUDE/header11.h
@@ -0,0 +1,16 @@
+/*
+ * header11.h
+ *
+ *  Created on: Sep 9, 2022
+ *      Author: np70301
+ */
+
+#ifndef HEADER11_H_
+#define HEADER11_H_
+
+int sum(int a , int b){
+	int c = a+b;
+	return c;
+}
+
+#endif /* HEADER11_H_ */
