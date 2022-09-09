d543bd67cf41c0cb93a7bcfcaa059b959b9b3f5a
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-09-09 14:26:54 +0530

    Create subtract.h

---
+++ WEEK-3/friday/HEADERFILE-INCLUDE/subtract.h
@@ -0,0 +1,17 @@
+/*
+ * subtract.h
+ *
+ *  Created on: Sep 9, 2022
+ *      Author: np70301
+ */
+
+#ifndef SUBTRACT_H_
+#define SUBTRACT_H_
+
+int sub(int a,int b){
+	int c = a-b;
+	return c;
+}
+
+
+#endif /* SUBTRACT_H_ */
